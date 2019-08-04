22970f0fba6385716f3a538e206e5426cb6cf336
James Wu <jameswu62@yahoo.co.nz>
2019-08-04 21:51:06 +1200

    add simple user interface

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from resistor_solver import ResistorSolver
 
-print("hello, world!")
+VER_MAJOR = 0
+VER_MINOR = 1
+AUTHOR = "James Wu"
+EMAIL = "jameswu62@yahoo.co.nz"
 
-solver = ResistorSolver(target=5.8, tolerance=0.005)
+print("Resistor Solver, v%i.%i, by %s - %s" % (VER_MAJOR, VER_MINOR, AUTHOR, EMAIL))
 
-result = solver.solution()
-
-for item in result:
-    print(item)
+try:
+    target = float(input("Please enter target value:"))
+    tolerance = float(input("Please enter tolerance:"))
+    solver = ResistorSolver(target=target, tolerance=tolerance)
+    result = solver.solution()
+    for item in result:
+        print(item)
+except ValueError:
+    print("Wrong value enter.")
