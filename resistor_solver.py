850d1b7b5632793ee3dd63fce0d4396646ca9641
James Wu <jameswu62@yahoo.co.nz>
2019-08-04 20:50:55 +1200

    add comments again and simplified stuff a little

--- resistor_solver.py
+++ resistor_solver.py
@@ -7,27 +7,31 @@ class ResistorSolver:
     find a solution to for the non-standard resistor values
     """
     _target = Resistor()
-    _tolerance = 0.0
-    _extension = ""
     _dataset_list = list()
 
     def __init__(self, target, tolerance=0.05, extension=".resistor"):
         """
         create Resistor solver object, with the target values and tolerance
-        :param target: target resistor value want to get
+        :param target: target resistor value
         :param tolerance: acceptable tolerance
         """
         self._target = Resistor(val=target, tolerance=tolerance)
-        self._tolerance = tolerance
-        self._extension = extension
         # extract the data from the data set file and put them into a data set list
         self._dataset_list = ResistorData(extension).get_dataset()
 
     def solution(self):
+        """
+
+        :return:
+        """
         result = list()
+        # loop through all data set
         for data_set in self._dataset_list:
+            # loop through all the values
+            index = 0
             for val_1 in data_set.data():
-                for val_2 in data_set.data():
+                index = index + 1
+                for val_2 in data_set.data()[index:]:
                     r1 = Resistor(val=val_1)
                     r2 = Resistor(val=val_2)
                     rp = r1.parallel_with(r2)
