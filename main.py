bfd67428554a7850367af5f35b107ab0591ac8be
James Wu <james@miracle.co.nz>
2019-08-06 09:30:04 +1200

    sort result before print

--- main.py
+++ main.py
@@ -13,10 +13,18 @@ print("Available dataset:", end=' ')
 print(*name)
 
 try:
+    # get user inputs
     target = float(input("Please enter target value:"))
     tolerance = float(input("Please enter tolerance:"))
+
+    # find a solution
     solver = ResistorSolver(target=target, tolerance=tolerance)
     result = solver.solution()
+
+    # sort output according to error
+    result.sort(key=lambda x :abs(x[4]))
+
+    # print result
     for item in result:
         print("%s, R1=%f, R2=%.2f, RParallel=%.4f, error=%.4f" % (item[0], item[1], item[2], item[3], item[4]))
 except ValueError:
